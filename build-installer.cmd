f62f1ae29e59ce88beef3da07675f0e2789edf9b
Aliaksandr Hrynko <san4o.mnk@gmail.com>
2023-04-29 19:09:19 +0300

    update libs

--- build-installer.cmd
+++ build-installer.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set msbuild="c:\Program Files\Microsoft Visual Studio\2022\Professional\Msbuild\Current\Bin\MSBuild.exe"
+set msbuild="c:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\MSBuild.exe"
 
 %msbuild% Installer\Installer.wixproj /m /t:build /p:Configuration="Release" /p:SolutionDir=%CD%
 
